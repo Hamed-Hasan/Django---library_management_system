ef0ac94d79803fab35e9b19e5393188d8016be48
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-12 19:24:08 +0300

    🔧 Added BookListView for displaying list of books. 📖 Implemented BookDetailView for showing details of a book. 💬 Created ReviewCreateView for adding reviews to books. 📝 Integrated ReviewForm for capturing review details. 🌐 Configured success_url to redirect after review submission.

--- library/views.py
+++ library/views.py
@@ -1,13 +1,26 @@
-
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from .models import Book
+from transaction.models import Review
+from .forms import ReviewForm
 
 class BookListView(ListView):
     model = Book
-    context_object_name = 'books'
     template_name = 'library/book_list.html'
+    context_object_name = 'books'
 
 class BookDetailView(DetailView):
     model = Book
-    context_object_name = 'book'
     template_name = 'library/book_detail.html'
+    context_object_name = 'book'
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'library/add_review.html'
+    success_url = reverse_lazy('library:book_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.book_id = self.kwargs['pk']
+        return super().form_valid(form)
