d575f0c6da02d5809acfdd55bff2fdef39d964ec
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-12 18:12:50 +0300

    🔧 Added Category model for database representation. 📘 Implemented Book model with fields for title, description, image, and borrowing price. 🖼️ Added ImageField to Book model for cover images. 💰 Set borrowing price field in Book model for pricing. 🔗 Established Many-to-Many relationship between Category and Book models.

--- library/models.py
+++ library/models.py
@@ -1,3 +1,19 @@
+# library/models.py
+
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='book_images/') 
+    borrowing_price = models.DecimalField(max_digits=6, decimal_places=2)
+    categories = models.ManyToManyField(Category, related_name='books')
+
+    def __str__(self):
+        return self.title
