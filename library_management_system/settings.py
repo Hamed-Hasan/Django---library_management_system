5c2856b1b71a25e5500df6df647399bb14119bcb
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-13 09:38:01 +0300

    📝 Added footer links section in footer.html. 🔒 Implemented legal links for Terms and Privacy. 🚀 Integrated social media links for Facebook, Instagram, and Twitter. 🏢 Added company-related links for Official Merchandise and Reviews. 🎉 Updated copyright year to 2024 in footer.

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library',
-    'transaction'
+    'transaction',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+   
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
