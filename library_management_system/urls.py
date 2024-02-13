6ad31b3656e64a9ce310a6baa9fff633d71c42db
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-13 10:24:57 +0300

    🛠️ Implemented account creation functionality. 🔐 Enhanced security measures for user authentication. ✨ Added profile customization options. 📧 Integrated email verification for new accounts.

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import include, path
-
+from core.views import HomeView
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
     path('transaction/', include('transaction.urls')),
