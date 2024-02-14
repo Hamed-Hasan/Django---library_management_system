95ed8c2123ad46fd9b3b97703f4154c8159619d2
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-14 09:08:40 +0300

    🛠️ Implemented user registration functionality in UserRegisterView. 🔐 Added UserProfile creation upon user registration. 🔄 Refactored form handling logic for better readability. 🖋️ Implemented UserProfileUpdateView for user profile updates. 📝 Set default balance value upon user creation in signals.py.

--- library/urls.py
+++ library/urls.py
@@ -4,7 +4,7 @@ from .views import BookListView, BookDetailView, ReviewCreateView
 app_name = 'library'  # This is for namespacing URLs
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'),  # Lists all books
+    path('', BookListView.as_view(), name='book_list'),
     path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),  # Shows details for a single book
     path('book/<int:pk>/review/', ReviewCreateView.as_view(), name='book_review'),  # Adds a review for a book
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
-from .models import Book
+from .models import Book, Category
 from transaction.models import Review
 from .forms import ReviewForm
 
@@ -8,6 +8,20 @@ class BookListView(ListView):
     model = Book
     template_name = 'library/book_list.html'
     context_object_name = 'books'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.GET.get('category')
+        if category:
+            queryset = queryset.filter(categories__name=category)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
 
 class BookDetailView(DetailView):
     model = Book
