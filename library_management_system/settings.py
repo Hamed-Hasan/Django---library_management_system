a60de63617438bbf6645521a71348caa98352e0f
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-15 11:32:07 +0300

    🔒 Configured Django settings for project initialization. 📂 Initialized environment variables for settings. 🛠️ Set up middleware for handling security and sessions. 🔑 Added password validation settings for user authentication. 🕒 Configured internationalization and time zone settings. ⚙️ Set up static files and media handling configurations.

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1']
 
 
 # Application definition
