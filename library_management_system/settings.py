48d829ea7c9fe0cdc0bb5203c67c5a2500f6693b
Hamed Hasan <hamedhasan422421@gmail.com>
2024-02-15 12:29:23 +0300

    🔒 Configured Django settings for project initialization. 📂 Initialized environment variables for settings. 🛠️ Set up middleware for handling security and sessions. 🔑 Added password validation settings for user authentication. 🕒 Configured internationalization and time zone settings. ⚙️ Set up static files and media handling configurations.

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -170,12 +170,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
    
-# ]
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
